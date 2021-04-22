930c7d15316a4919f10b6b6c912a33128deae043
Игорь Шашков <compvirusa@gmail.com>
2021-04-22 11:14:54 +0300

    UP_22_04

---
+++ Homework_UP/FormatStrings/Formatting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatStrings
+{
+    class Formatting
+    {
+        static public string Calculate(string value, List<string> subStrings)
+        {
+            string result = string.Copy(value);
+
+            foreach (string subString in subStrings)
+            {
+                string tempValue = value.Replace(subString, subString.ToUpper());
+
+                string tempRes = "";
+                for (int letter = 0; letter < result.Length; ++letter)
+                {
+                    if (tempValue[letter].ToString().ToUpper() == tempValue[letter].ToString())
+                    {
+                        tempRes += tempValue[letter];
+                    }
+                    else
+                    {
+                        tempRes += result[letter];
+                    }
+                }
+                result = string.Copy(tempRes);
+            }
+
+            return result;
+        }
+    }
+}
