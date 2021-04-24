3d0bbf1c0afd61b7c83fe3490114743030ab03f1
Roickard <afanasyev.andrey2002@gmail.com>
2021-04-24 14:41:57 +0300

    ISIT_23_04

---
+++ Homework_ISIT/MarketData/IMarket.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MarketData
+{
+	class IMarket
+	{
+		private readonly SqlConnection sqlConnection;
+		private readonly SqlCommand sqlCommand;
+		private readonly SqlDataAdapter adapter;
+		private readonly DataTable dataTable;
+		public IMarket(DataTable dataTable)
+		{
+			try
+			{
+				sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + AppDomain.CurrentDomain.BaseDirectory + @"Market.mdf;Integrated Security=True");
+				sqlCommand = new SqlCommand("", sqlConnection);
+				adapter = new SqlDataAdapter(new SqlCommand("SELECT * FROM [dbo].[Products];", sqlConnection));
+				FillAdapter();
+				this.dataTable = dataTable;
+				sqlConnection.Open();
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message, ex.Source, MessageBoxButton.OK, MessageBoxImage.Error);
+				Application.Current.MainWindow.Close();
+			}
+		}
+		private void FillAdapter()
+		{
+			adapter.InsertCommand = new SqlCommand("InsertProduct", sqlConnection);
+			adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+			adapter.InsertCommand.Parameters.Add(new SqlParameter("@name", SqlDbType.NVarChar, 50, "Name"));
+			adapter.InsertCommand.Parameters.Add(new SqlParameter("@amount", SqlDbType.Int, 0, "Amount"));
+			adapter.InsertCommand.Parameters.Add(new SqlParameter("@price", SqlDbType.Int, 0, "Price"));
+			SqlParameter totalPriceParam = adapter.InsertCommand.Parameters.Add("@totalPrice", SqlDbType.Int, 0, "TotalPrice");
+			totalPriceParam.Direction = ParameterDirection.Output;
+			SqlParameter idParam = adapter.InsertCommand.Parameters.Add("@id", SqlDbType.Int, 0, "Id");
+			idParam.Direction = ParameterDirection.Output;
+		}
+		public void UpdateData()
+		{
+			new SqlCommandBuilder(adapter);
+			adapter.Update(dataTable);
+			dataTable.Clear();
+			adapter.Fill(dataTable);
+		}
+		public string GetTotalSum()
+		{
+			sqlCommand.CommandText = $"SELECT SUM([TotalPrice]) FROM [Products];";
+			return sqlCommand.ExecuteScalar().ToString();
+		}
+	}
+}
---
+++ Homework_ISIT/MarketData/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MarketData
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private readonly IMarket market;
+		private readonly DataTable dataTable;
+		public MainWindow()
+		{
+			InitializeComponent();
+			dataTable = new DataTable();
+			market = new IMarket(dataTable);
+		}
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			MarketGrid.ItemsSource = dataTable.DefaultView;
+			UpdateProducts();
+		}
+		private void UpdateProducts()
+		{
+			market.UpdateData();
+			Sum.Content = "Sum: " + market.GetTotalSum();
+		}
+		private void AddProduct(object sender, RoutedEventArgs e)
+		{
+			UpdateProducts();
+		}
+		private void DeleteProduct(object sender, RoutedEventArgs e)
+		{
+			if (MarketGrid.SelectedItems != null)
+			{
+				for (int selectedProduct = 0; selectedProduct < MarketGrid.SelectedItems.Count; ++selectedProduct)
+				{
+					DataRowView rowView = MarketGrid.SelectedItems[selectedProduct] as DataRowView;
+					if (rowView != null)
+					{
+						rowView.Row.Delete();
+						--selectedProduct;
+					}
+				}
+			}
+
+			UpdateProducts();
+		}
+	}
+}
