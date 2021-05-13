61924d1e4c8df30545da6ef0cb55d524bbbecc57
Roickard <afanasyev.andrey2002@gmail.com>
2021-05-13 19:32:16 +0300

    ISIT, TIS

---
+++ Homework_TIS/WeightConverter/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WeightConverter
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private List<double> Coefficients = new List<double>(5) { 0.002205, 0.00006105, 0.035274, 0.564383, 15.43236 };
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+		private void SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			FillResult();
+		}
+		private void tbChanging(object sender, KeyEventArgs e)
+		{
+			FillResult();
+		}
+		private void FillResult()
+		{
+			try
+			{
+				lbResult.Items.Clear();
+				foreach (double weight in SplitWeights())
+				{
+					string type = (cbConverterType.SelectedItem as TextBlock).Text;
+					string abbr = type.Substring(type.Length - 3, 2);
+					lbResult.Items.Add($"{weight} g = {Convert(weight, cbConverterType.SelectedIndex)} {abbr}");
+				}
+			}
+			catch (Exception) { }
+		}
+		private IEnumerable<double> SplitWeights()
+		{
+			string[] weigths = tbWeight.Text.Split(' ');
+			foreach(string weight in weigths)
+			{
+				if(double.TryParse(weight, out double result))
+				{
+					yield return result;
+				}
+			}
+		}
+		private void SwitchStyle(object sender, RoutedEventArgs e)
+		{
+			lbResult.FontStyle = (cbItalic.IsChecked.Value) ? (FontStyles.Italic) : (FontStyles.Normal);
+			lbResult.Foreground = (cbRed.IsChecked.Value) ? (Brushes.Red) : (Brushes.Black);
+		}
+		private double Convert(double source, int type)
+		{
+			double result = 0.0;
+			try
+			{
+				result = source * Coefficients[type];
+			}
+			catch(Exception) {}
+
+			return result;
+		}
+	}
+}
---
+++ Homework_TIS/ZNAKApp/ZNAK.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Collections;
+using System.Globalization;
+
+namespace ZNAKApp
+{
+	enum Signs : byte
+	{
+		Aries,
+		Taurus,
+		Gemini,
+		Cancer,
+		Leo,
+		Virgo,
+		Libra,
+		Scorpio,
+		Sagittarius,
+		Capricorn,
+		Aquarius,
+		Pisces,
+		None
+	}
+	class AstroSign
+	{
+		private const byte SIGNS_CAPACITY = 12;
+		private static Hashtable hashTabe;
+		static AstroSign()
+		{
+			hashTabe = new Hashtable(SIGNS_CAPACITY);
+			hashTabe.Add(Signs.Aries, 21);
+			hashTabe.Add(Signs.Taurus, 20);
+			hashTabe.Add(Signs.Gemini, 21);
+			hashTabe.Add(Signs.Cancer,21);
+			hashTabe.Add(Signs.Leo, 23);
+			hashTabe.Add(Signs.Virgo, 23);
+			hashTabe.Add(Signs.Libra, 23);
+			hashTabe.Add(Signs.Scorpio, 23);
+			hashTabe.Add(Signs.Sagittarius, 23);
+			hashTabe.Add(Signs.Capricorn, 22);
+			hashTabe.Add(Signs.Aquarius, 20);
+			hashTabe.Add(Signs.Pisces, 19);
+		}
+		public static Signs GetSign(DateTime date)
+		{
+			int signDate = (int)hashTabe[(Signs)((date.Month + 9) % SIGNS_CAPACITY)];
+			int prevDate = (int)hashTabe[(Signs)((date.Month + 8) % SIGNS_CAPACITY)];
+			if (signDate <= date.Day && prevDate > date.Day)
+			{
+				return (Signs)((date.Month + 9) % SIGNS_CAPACITY);
+			}
+			else
+			{
+				return (Signs)((date.Month + 8) % SIGNS_CAPACITY);
+			}
+		}
+	}
+    class ZNAK : IComparable
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public Signs Sign { get; set; }
+		public DateTime Date { get; set; }
+		public ZNAK()
+		{
+			FirstName = "";
+			SecondName = "";
+			Sign = Signs.None;
+			Date = DateTime.MinValue;
+		}
+		public void SetData()
+		{
+			
+			FirstName = SetField("First name: ");
+			SecondName = SetField("Second name: ");
+			Date = ParseDate();
+			Sign = AstroSign.GetSign(Date);
+		}
+		private string SetField(string message)
+		{
+			Console.Write(message);
+			return Console.ReadLine();
+		}
+		private DateTime ParseDate()
+		{
+			DateTime date;
+			while (true)
+			{
+				Console.Write("Date: ");
+				if (DateTime.TryParse(Console.ReadLine(), out date))
+				{
+					break;
+				}
+			}
+			return date;
+		}
+        public string Serrialize()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+        public static ZNAK Desserialize(string json)
+        {
+            return JsonSerializer.Deserialize<ZNAK>(json);
+        }
+		public override string ToString()
+		{
+			return $"First name: {FirstName}\n" +
+				   $"Second name: {SecondName}\n" +
+				   $"Sign: {Sign}\n" +
+				   $"Date: {Date.ToShortDateString()}";
+		}
+
+		public int CompareTo(object obj)
+		{
+			ZNAK znak = obj as ZNAK;
+			return Sign.CompareTo(znak.Sign);
+		}
+	}
+}
